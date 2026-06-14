88fd6c0885e99519fde483a3913cfe4b2aa8cc07
Aymen Memni <memni.aymen@gmail.com>
2026-06-15 00:15:16 +0200

    Add substitution cipher

--- Security/cryptography-in-python/Permutations.py
+++ Security/cryptography-in-python/Permutations.py
@@ -1,10 +1,26 @@
-from itertools import permutations
+# from itertools import permutations
 
-my_list = [1, 2, 3, 4, 5, 6]
+# my_list = [1, 2, 3, 4, 5, 6]
 
-list_of_permutations = permutations(my_list)
-cnt = 0
-for permutation in list_of_permutations:
-    cnt += 1
-    print("Permutation number {}: {}".format(cnt, permutation))
-print("Number of permutations: {}".format(cnt))
+# list_of_permutations = permutations(my_list)
+# cnt = 0
+# for permutation in list_of_permutations:
+#     cnt += 1
+#     print("Permutation number {}: {}".format(cnt, permutation))
+# print("Number of permutations: {}".format(cnt))
+
+import cProfile
+
+def faculty(n):
+    if n <= 1:
+        return 1
+    else:
+        return faculty(n-1) * n
+    
+def counter(n):
+    cnt = 0
+    for i in range(n):
+        cnt += 1
+    return cnt
+
+cProfile.run("counter(faculty(13))")
---
+++ Security/cryptography-in-python/SubstitutionCipher.py
@@ -0,0 +1,24 @@
+import random
+
+def generate_key():
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    cletters = list(letters)
+    key = {}
+    for c in letters:
+        key[c] = cletters.pop(random.randint(0, len(cletters) - 1))
+    return key
+
+def encrypt(key, message):
+    cipher = ""
+    for c in message:
+        if c in key:
+            cipher += key[c]
+        else:
+            cipher += c
+    return cipher
+
+key = generate_key()
+print("Key: {}".format(key))
+message = "YOU ARE AWESOME"
+cipher = encrypt(key, message)
+print("Cipher: {}".format(cipher))
